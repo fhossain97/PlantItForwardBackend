a990c630ab7aca9b9c8779a449c8476f065f3b94
justaples <juhpaim@hotmail.com>
2022-07-23 13:30:12 -0400

    Item routes and controller updated - added RESTful functionality - tested with Postman - all ok

--- pif-backend/routes/itemRoutes.js
+++ pif-backend/routes/itemRoutes.js
@@ -1,8 +1,11 @@
-//ok
 const router = require('express').Router()
 const itemCtrl = require('../controllers/itemControllers')
 
 router.get('/', itemCtrl.index)
+router.get('/:id', itemCtrl.showOneItem)
+router.post('/', itemCtrl.createNewItem)
+router.put('/:id', itemCtrl.updateItem)
+router.delete('/:id', itemCtrl.deleteItem)
 
 
 module.exports = router 
