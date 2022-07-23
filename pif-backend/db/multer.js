0102df3018fbc4ffd93364f315ed3e64c96dfc46
fhossain <farhana.hossainse@gmail.com>
2022-07-23 18:38:50 -0400

    multer code added

--- pif-backend/db/multer.js
+++ pif-backend/db/multer.js
@@ -2,10 +2,10 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../images");
+    cb(null, "./public");
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    cb(null, new Date().toISOString() + "-" + file.originalname);
   },
 });
 
--- pif-backend/routes/itemRoutes.js
+++ pif-backend/routes/itemRoutes.js
@@ -1,11 +1,13 @@
-const router = require('express').Router()
-const itemCtrl = require('../controllers/itemControllers')
+const router = require("express").Router();
+const itemCtrl = require("../controllers/itemControllers");
+const upload = require("../db/multer");
 
-router.get('/', itemCtrl.index)
-router.get('/:id', itemCtrl.showOneItem)
-router.post('/', itemCtrl.createNewItem)
-router.put('/:id', itemCtrl.updateItem)
-router.delete('/:id', itemCtrl.deleteItem)
+router.get("/new", upload.single("Image"), itemCtrl.newRoute);
+router.get("/", itemCtrl.index);
+router.get("/:id", itemCtrl.showOneItem);
+router.get("/:id/edit", upload.single("Image"), itemCtrl.editRoute);
+router.post("/", upload.single("Image"), itemCtrl.createNewItem);
+router.put("/:id", upload.single("Image"), itemCtrl.updateItem);
+router.delete("/:id", itemCtrl.deleteItem);
 
-
-module.exports = router 
+module.exports = router;
