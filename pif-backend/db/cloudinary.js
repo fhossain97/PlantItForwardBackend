bf6eb2091a015351c318700cc6d82bcd8aa17ba9
fhossain <farhana.hossainse@gmail.com>
2022-07-26 23:14:22 -0400

    added owner

--- pif-backend/db/cloudinary.js
+++ pif-backend/db/cloudinary.js
@@ -6,40 +6,42 @@ const {CloudinaryStorage} = require('multer-storage-cloudinary')
 dotenv.config();
 
 const {
-    CLOUDINARY_HOST,
+    CLOUDINARY_NAME,
     CLOUDINARY_API_KEY,
     CLOUDINARY_API_SECRET
 } = process.env
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-// exports.uploads = (file, folder) => {
-//   console.log(process.env);
-//   return new Promise((resolve) => {
-//     cloudinary.uploader.upload(
-//       file,
-//       (result) => {
-//         resolve({
-//           url: result.url,
-//           id: result.public_id,
-//         });
-//       },
-//       {
-//         resource_type: "auto",
-//         folder: folder,
-//       }
-//     );
-//   });
-// };
+
+
+// // exports.uploads = (file, folder) => {
+// //   console.log(process.env);
+// //   return new Promise((resolve) => {
+// //     cloudinary.uploader.upload(
+// //       file,
+// //       (result) => {
+// //         resolve({
+// //           url: result.url,
+// //           id: result.public_id,
+// //         });
+// //       },
+// //       {
+// //         resource_type: "auto",
+// //         folder: folder,
+// //       }
+// //     );
+// //   });
+// // };
 
 const storage = new CloudinaryStorage ({
     cloudinary: cloudinary,
     params: {
-        folder: "folder name",
+        folder: "public",
         format: async () => 'jpg', 
         public_id: (req, file) => file.filename,
     }
