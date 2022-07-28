d04eab4c6f50fdacc6b5cb720f3a024bbe243f32
fhossain <farhana.hossainse@gmail.com>
2022-07-27 20:05:20 -0400

    getting somehwere

--- pif-backend/routes/itemRoutes.js
+++ pif-backend/routes/itemRoutes.js
@@ -2,21 +2,14 @@ const router = require("express").Router();
 const itemCtrl = require("../controllers/itemControllers");
 const upload = require("../db/multer");
 
-// router.get("/new", upload.single("images"), itemCtrl.newRoute);
-// router.get("/", itemCtrl.index);
-// router.get("/:id", itemCtrl.showOneItem);
-// router.get("/:id/edit", upload.single("images"), itemCtrl.editRoute);
-// router.post("/", upload.single("images"), itemCtrl.createNewItem);
-// router.put("/:id", upload.single("images"), itemCtrl.updateItem);
-// router.delete("/:id", itemCtrl.deleteItem);
-
-
-router.get("/new", itemCtrl.newRoute);
+router.get("/new", upload.single("images"), itemCtrl.newRoute);
 router.get("/", itemCtrl.index);
 router.get("/:id", itemCtrl.showOneItem);
-router.get("/:id/edit", itemCtrl.editRoute);
-router.post("/", itemCtrl.createNewItem);
-router.put("/:id", itemCtrl.updateItem);
+router.get("/:id/edit", upload.single("images"), itemCtrl.editRoute);
+router.post("/", upload.single("images"), itemCtrl.createNewItem);
+router.put("/:id", upload.single("images"), itemCtrl.updateItem);
 router.delete("/:id", itemCtrl.deleteItem);
+
+
 
 module.exports = router;
