1ab012fcd786a5dbf1def4a44821bdead5772208
patientdevtech <patientdevtech@gmail.com>
2022-08-02 20:44:10 -0400

    plant items reseeded

--- pif-backend/db/seeds.js
+++ pif-backend/db/seeds.js
@@ -6,7 +6,7 @@ const items =
         {   
             name: 'Lantern Flower - Lifesaver Plant',
             quantity: 2,
-            images: 'https://i.imgur.com/SIk0jGM.jpg',
+            images: 'https://i.imgur.com/AAI7uSr.jpg',
             description: 'Two available.  Each have baby flower buds!',
             genus: 'Ceropegia',
             status: '1 available' 
@@ -46,12 +46,81 @@ const items =
         {   
             name: 'Japanese Camellia',
             quantity: 5,
-            images: 'http://s3-us-west-2.amazonaws.com/cypdb/350w/camellia-japonica-flower.jpg',
+            images: 'https://i.imgur.com/ghmzbcC.jpg',
             description: 'There are many cultivars with flowers up to 6 inches in winter-spring. The colors are variable. The gardener needs to watch for scales, aphids, chewing insects and fungal diseases. It requires acid soil and will have problems if pH is too high.',
             genus: 'Camellia',
             status: '5 plants available' 
         },
-        
+        {   
+            name: 'Virginia Blue Fern',
+            quantity: 2,
+            images: 'https://i.imgur.com/rSoEYEF.jpg',
+            description: 'Get you some blue rabbits foot fern.  Its a great fern for a terrarium.',
+            genus: 'Phlebodium',
+            status: 'Just 2 plants available' 
+        },
+        {
+            name: 'Purple Amaranth',
+            quantity: 1,
+            images: 'https://i.imgur.com/3anRDNz.png?1',
+            description: 'Can you believe this beautiful plant is edible?  Eat the leaves or the grain.  Throw it in your salads and on your sandwiches!',
+            genus: 'Amaranthus',
+            status: 'Just 1 left' 
+        },
+        {   
+            name: 'Succulent Echeveria Propogates',
+            quantity: 50,
+            images: 'https://i.imgur.com/RaqLon9.jpg',
+            genus: 'Echeveria',
+            status: 'I have many propogates - let me know how many you need!' 
+        },
+        {   
+            name: 'Strawberry',
+            quantity: 5,
+            images: 'https://i.imgur.com/MwIFBkK.jpg',
+            description: 'There are many cultivars with flowers up to 6 inches in winter-spring. The colors are variable. The gardener needs to watch for scales, aphids, chewing insects and fungal diseases. It requires acid soil and will have problems if pH is too high.',
+            genus: 'Fragaria',
+            status: '5 plants available' 
+        },
+        {   
+            name: 'Mermaid Moss',
+            quantity: 12,
+            images: 'https://i.imgur.com/P7lFBCc.jpg',
+            description: 'My mermaid is growing too much moss.  Contact me if you need some.',
+            genus: 'Sphagnum',
+            status: 'Get yourself a clump.' 
+        },
+        {   
+            name: 'UFO plant',
+            quantity: 2,
+            images: 'https://i.imgur.com/pF8bwST.jpg',
+            description: 'Also called Chinese money plant, pancake plant, and missionary plant.  I have 2.',
+            genus: 'Pilea',
+            status: '1 plant still available' 
+        },
+        {   
+            name: 'Aloe Imma Plant',
+            quantity: 5,
+            images: 'https://i.imgur.com/3XWsSUV.jpg',
+            genus: 'Aloe',
+            status: '5 plants available' 
+        },
+        {   
+            name: 'Neon Pothos',
+            quantity: 4,
+            images: 'https://i.imgur.com/Qj9pOZ1.jpg',
+            description: 'Get it while its hot.',
+            genus: 'Epipremnum',
+            status: '4 plants available' 
+        },
+        {   
+            name: 'Polka Dot Begonia',
+            quantity: 1,
+            images: 'https://i.imgur.com/uxMSmCt.jpg',
+            description: 'The leaves are red underneath!',
+            genus: 'Begonia',
+            status: '1 plants available' 
+        },
     ]
 
 
