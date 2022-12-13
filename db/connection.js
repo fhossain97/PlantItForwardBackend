b822cd16bf8049f15e826ab0bb332fac9568b141
fhossain <farhana.hossainse@gmail.com>
2022-12-13 13:50:46 -0500

    trying again

--- db/connection.js
+++ db/connection.js
@@ -1,32 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
+require('dotenv').config();
 
 mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
 
-const db = mongoose.connection;
-
-db.on("connected", () => {
-  console.log(`Connected at ${db.host}:${db.port}`);
-});
-
-
-
-
-
-// const mongoose = require('mongoose')
-// require('dotenv').config();
-
-// console.log(process.env.DATABASE_URL);
-
-// mongoose.connect(
-//     process.env.DATABASE_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// })
-
-// .then(instance => {
-//     console.log(`Connected on ${instance.connections[0].name}`)
-// })
-// .catch(err => console.log(`Error! See details: ${err}`))
+.then(instance => {
+    console.log(`Connected on ${instance.connections[0].name}`)
+})
+.catch(err => console.log(`Error! See details: ${err}`))
